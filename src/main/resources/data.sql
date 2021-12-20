a2393ac1fefc25619a10494d2b34b5d1981c32ad
emersonope <emersonope@gmail.com>
2021-12-20 16:00:09 -0300

    autenticacao + bug fix

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$jsYzmY5BucVV5D2IRzL0BurNqkCB.O7grCDeamkLCHFEADc1Ezy.G');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
